58889d27a90b961064bf394799c16b5fbc782592
BobWritesCode <warwick_hart@hotmail.com>
2022-12-15 15:35:09 +0000

    Create class ServerListing and migrate

--- website/models.py
+++ website/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, 'Draft'), (1, 'Published'))
+
+class ServerListing(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+    short_description = models.TextField()
+    long_description = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="server_listings")
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    logo = CloudinaryField('image', default='placeholder')
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name="server_likes", blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
