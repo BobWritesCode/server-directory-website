37fa3ae737aea078f0fd1681a6c71d4bb2e3d9d3
BobWritesCode <warwick_hart@hotmail.com>
2022-12-16 17:24:36 +0000

    Update URL paths

--- server_directory/urls.py
+++ server_directory/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from website.views import get_directory_list
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', get_directory_list, name='directory_list'),
+    path('', include('website.urls'), name='website_urls'),
 ]
