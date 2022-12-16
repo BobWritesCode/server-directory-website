6a025cf62363575503a757e929c7550f2a75794c
BobWritesCode <warwick_hart@hotmail.com>
2022-12-16 17:27:46 +0000

    Add ServerListing, Tag and Game to admin view

--- website/admin.py
+++ website/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
-from .models import ServerListing
+from .models import ServerListing, Tag, Game
 
-admin.site.register(ServerListing)
+@admin.register(ServerListing)
+class ServerListingAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ('name',)
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ('name',)
