9d75d26e0b8bf5ef423ecc38745eca57f250b4c5
BobWritesCode <warwick_hart@hotmail.com>
2022-12-16 17:28:09 +0000

    Add Game and Tag model, update ServerListing

--- website/models.py
+++ website/models.py
@@ -4,12 +4,28 @@ from cloudinary.models import CloudinaryField
 
 STATUS = ((0, 'Draft'), (1, 'Published'))
 
+class Game(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+    slug = models.SlugField(max_length=20, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
 class ServerListing(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name="server_listings")
+    tags = models.ManyToManyField(Tag, blank=True)
     title = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50, unique=True)
     short_description = models.TextField()
     long_description = models.TextField()
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="server_listings")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="server_owner")
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     logo = CloudinaryField('image', default='placeholder')
@@ -22,6 +38,8 @@ class ServerListing(models.Model):
     def __str__(self):
         return self.title
 
+    def number_of_tags(self):
+        return self.tags.count()
+
     def number_of_likes(self):
         return self.likes.count()
-
