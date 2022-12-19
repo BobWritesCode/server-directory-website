a264159b3de5d2c10da4c55b150344f3f49adff9
BobWritesCode <warwick_hart@hotmail.com>
2022-12-19 14:02:31 +0000

    Add new fields to Game and ServerListing models
    - Add Discord to ServerListing
    - Add tags to Game

--- website/models.py
+++ website/models.py
@@ -4,16 +4,17 @@ from cloudinary.models import CloudinaryField
 
 STATUS = ((0, 'Draft'), (1, 'Published'))
 
-class Game(models.Model):
-    name = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=50, unique=True)
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+    slug = models.SlugField(max_length=20, unique=True)
 
     def __str__(self):
         return f"{self.name}"
 
-class Tag(models.Model):
-    name = models.CharField(max_length=20)
-    slug = models.SlugField(max_length=20, unique=True)
+class Game(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+    tags = models.ManyToManyField(Tag, blank=False)
 
     def __str__(self):
         return f"{self.name}"
@@ -23,13 +24,14 @@ class ServerListing(models.Model):
     tags = models.ManyToManyField(Tag, blank=True)
     title = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50, unique=True)
-    short_description = models.TextField()
-    long_description = models.TextField()
+    short_description = models.TextField(max_length=200)
+    long_description = models.TextField(max_length=2000)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="server_owner")
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     logo = CloudinaryField('image', default='placeholder')
     status = models.IntegerField(choices=STATUS, default=0)
+    discord =  models.CharField(max_length=50)
     likes = models.ManyToManyField(User, related_name="server_likes", blank=True)
 
     class Meta:
