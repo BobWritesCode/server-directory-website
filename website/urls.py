d6168626118d4122b59b4d4db0a221d36b46d124
BobWritesCode <warwick_hart@hotmail.com>
2023-01-08 16:18:31 +0000

    feat: display server when selected (WIP) #13

--- website/urls.py
+++ website/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('server-list/<slug>', views.ServerListings.as_view(), name='server-list'),
+    path('server/<slug>', views.ServerDetail.as_view(), name='server'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
 
 from django.shortcuts import render, get_object_or_404
+from django.views import View
 from django.views.generic.list import ListView
 from django.views.generic.base import TemplateView
-
 from website.models import ServerListing, Tag, Game
 
 def index(request):
@@ -15,7 +15,6 @@ def index(request):
 class ServerListings(ListView):
 
     def get(self, request, slug, *args, **kwargs):
-
         game = get_object_or_404(Game, slug=slug)
         queryset = ServerListing.objects.filter(status=1, game=game).order_by('-created_on')
 
@@ -24,5 +23,20 @@ class ServerListings(ListView):
             "server-list.html",
             {
                 "serverlisting": queryset,
+            },
+        )
+
+
+class ServerDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = ServerListing.objects.filter(status=1)
+        server = get_object_or_404(queryset, slug=slug)
+
+        return render(
+            request,
+            "server_detail.html",
+            {
+                "server": server,
             },
         )
