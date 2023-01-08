f5253366b09988c2923be83470896987dd32a5b9
BobWritesCode <warwick_hart@hotmail.com>
2023-01-08 15:15:50 +0000

    chore: update homepage/index url

--- website/urls.py
+++ website/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home'),
+    path('', views.index, name='home'),
     path('server-list/<slug>', views.ServerListings.as_view(), name='server-list'),
 ]
