446e62af056d54d2e6d6ec12194f0d72c6f74fc5
BobWritesCode <warwick_hart@hotmail.com>
2023-01-10 16:24:01 +0000

    feat: implement crispy into project

--- server_directory/settings.py
+++ server_directory/settings.py
@@ -53,8 +53,13 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-    'website'
+    'website',
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
