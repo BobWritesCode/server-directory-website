58f7ae5469466b43802b01be63227be1a37d847f
BobWritesCode <warwick_hart@hotmail.com>
2023-01-29 17:51:11 +0000

    chore: reset database so can use new user model

--- website/models.py
+++ website/models.py
@@ -7,16 +7,16 @@ STATUS = ((0, 'Draft'), (1, 'Published'))
 
 
 class CustomUser(AbstractUser):
-    username = models.CharField(max_length=20, unique=True)
+    username = models.CharField(max_length=20, unique=True, blank=False)
     first_name = models.CharField(max_length=20)
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, blank=False)
     email_verified = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
 
     class Meta:
         db_table = 'auth_user'
