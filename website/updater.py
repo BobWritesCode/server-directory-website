572f97c73bb3604d28b7f56794f5f65331c7d636
BobWritesCode <warwick_hart@hotmail.com>
2023-02-13 19:00:27 +0000

    chore: complete lint suggestions (see more)
    Lint used was pylint-django and pycodestyle

--- website/updater.py
+++ website/updater.py
@@ -1,10 +1,16 @@
+"""
+Initiates task intervals.
+Tasks found in jobs.py
+"""
+
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from .jobs import daily_jobs
 
+
 def start():
+    """ Run when project starts """
     scheduler = BackgroundScheduler()
     # add_job() Runs at midnight every night.
-    scheduler.add_job(daily_jobs, 'cron', hour ='0')
+    scheduler.add_job(daily_jobs, 'cron', hour='0')
     scheduler.start()
-
