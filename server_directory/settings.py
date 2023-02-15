e6edb5abeb8064aabd4483a81f669d7a3f52fb61
BobWritesCode <warwick_hart@hotmail.com>
2023-02-15 22:00:52 +0000

    doc: switch debug to False

--- server_directory/settings.py
+++ server_directory/settings.py
@@ -26,7 +26,7 @@ user_localhost = False
 user_local_db = False
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
