3d09c68c3642d0f2eb0569de572ad789e05e82fa
BobWritesCode <warwick_hart@hotmail.com>
2023-02-17 15:21:54 +0000

    chore: change default to False for is_active

--- website/models.py
+++ website/models.py
@@ -52,7 +52,7 @@ class CustomUser(AbstractUser):
     email = models.EmailField(unique=True, blank=False)
     email_verified = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     is_banned = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
 
